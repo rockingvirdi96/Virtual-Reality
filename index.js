ac5cf25d89e9c72f1ec5bb0bb7818b018e61c58b
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2020-01-13 01:21:27 -0500

    Toggle on action UPDATE

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 $ = (e) => {
     return document.querySelector(e);
 }
-
-// }
-
-// animate = () => {
-//     requestAnimationFrame(animate);
-// }
-
-// requestAnimationFrame(animate);`
 $('a-box').setAttribute('rotation', '0 45 0');
-$('a-box').addEventListener('click', () => {
 
-    $('a-box').setAttribute('animation', 'property: rotation;from: 0 0 0; to: 0 360 0; dur: 10000; easing: linear;loop: true ');
+
+// $('a-box').setAttribute('animation', '');
+$('a-box').addEventListener('click', () => {
+    var from = $('a-box').getAttribute('rotation').y;
+    var animation = $('a-box').getAttribute('animation');
+    if (!(animation.property == "rotation")) {
+        $('a-box').setAttribute('animation', 'property: rotation;from: 0 ' + from + ' 0 ; to: 0 ' + (360 + from) + ' 0;dur:3000; easing: linear;loop: true ');
+        console.log(from);
+    }
+    else if (animation.property == "rotation") {
+        $('a-box').removeAttribute('animation');
+        $('a-box').setAttribute('animation', '');
+    }
 });
